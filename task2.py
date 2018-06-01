2880c2a92de389ddfde45a5ec35c0b9a9465559e
TG-Techie <jonahy-m@Jonahs-MacBook-Pro.local>
2018-05-31 20:28:07 -0400

    finished prelim task: task_list is likea  tri thread computer list that can have prioirty for tasks and ecitues by priority than time order added

--- task2.py
+++ task2.py
@@ -7,34 +7,48 @@ def examp_func(arg, times, flavor):
         print("""he who wish to cross the bridge of death must andswer me these
 questions three, ere the other side he C...  (++)""")
 
+TEST_VAL = True
+
+def test(x):
+    if TEST_VAL:
+        print(x)
 
 class task_list():
 
-    
     def __init__(self):
+        self.list1 = []
+        self.list2 = []
+        self.list3 = []
+        
+    def add_task(self,func, arg_tup, priority):
+        if priority == 1:
+            self.list1.append(task(func, arg_tup))
+            print('1',self.list1)
+        elif priority ==2 :
+            self.list2.append(task(func, arg_tup))
+            print('2',self.list2)
+        else:
+            self.list3.append(task(func, arg_tup))
+            print('3',self.list3)
+
+    def chug(self):
+        for cur_list in (self.list1,self.list2,self.list3):
+            test(cur_list)
+            for i in cur_list:
+                test(i.arg_tup)
+                i.perform()
 
 
 class task():
 
     
-    def __init__(self,func, arg_tup, priority):
+    def __init__(self,func, arg_tup):
         self.func = func
-        self.arg_tup = arg_tup
+        # set arg_tup as a tup whether or not inputed as tup or not
+        if type(arg_tup) == tuple:
+            self.arg_tup = arg_tup
+        else:
+            self.arg_tup = (arg_tup,)
 
     def perform(self):
-        self.func( *self.arg_tup)
-
-
-
-
-#make task list  
-task_list = []
-
-#making a task called shmurp
-shmurp = task(examp_func,("hi",3,True))
-
-#add shmurp task to task list
-task_list.append(shmurp)
-
-for i in task_list:
-    i.perform()
+        self.func(*(self.arg_tup))
