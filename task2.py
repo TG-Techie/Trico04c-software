f845ce14020880c7db82ab7e028d38b3e82fc26d
TG-Techie <jonahy-m@Jonahs-MacBook-Pro.local>
2018-05-31 20:28:39 -0400

    finished prelim task: task_list is likea  tri thread computer list that can have prioirty for tasks and ecitues by priority than time order added

--- task2.py
+++ task2.py
@@ -52,3 +52,25 @@ class task():
 
     def perform(self):
         self.func(*(self.arg_tup))
+
+
+test('is test warking / on ?')
+
+marp = task(print, 'must answer')
+marp.perform()
+
+blap = task_list()
+blap.add_task(print,'he who wish to cross...',3)
+blap.chug()'''
+
+#make task list  
+task_list = []
+
+#making a task called shmurp
+shmurp = task(examp_func,("hi",3,True))
+
+#add shmurp task to task list
+task_list.append(shmurp)
+
+for i in task_list:
+    i.perform()"""
